63efa89e1f633b2fcda9fdc4cfecb9b472e8149f
mehnazimu <mehnaz.imu@gmail.com>
2022-11-27 14:37:02 +0600

    verifyjwt

--- index.js
+++ index.js
@@ -128,6 +128,26 @@ async function run() {
             res.send(result)
         });
 
+        app.get('/bookings',verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+
+            if (email !== decodedEmail) {
+                return res.status(403).send({ massage: 'forbidden access' });
+            }
+
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        });
+
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingsCollection.findOne(query);
+            res.send(booking);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
